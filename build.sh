33cd1e5ce460a0a4c2f00b5babe3f2ff3b95c707
Lukas Werling <lukas@lwrl.de>
2025-12-20 16:39:19 +0100

    Remove unnecessary files from the template

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
-IMAGE=glove80-zmk-config-docker
-BRANCH="${1:-main}"
-
-docker build -t "$IMAGE" .
-docker run --rm -v "$PWD:/config" -e UID="$(id -u)" -e GID="$(id -g)" -e BRANCH="$BRANCH" "$IMAGE"
+nix-build "$1" -o "result-$1"
